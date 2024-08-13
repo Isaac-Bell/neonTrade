44c61069bc9dc46553eaede801075ab5448c55ec
Hamiora Keelan <Hamiora.Keelan@gmail.com>
2024-08-13 20:26:22 +1200

    add seeds

--- server/db/seeds/00_accounts.js
+++ server/db/seeds/00_accounts.js
@@ -1,13 +1,11 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = async function(knex) {
+export async function seed(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('account').del();
+
+  // Inserts seed entries
+  await knex('account').insert([
+    { account: 'account1', token: null, currency: 'usd' },
+    { account: 'account2', token: null, usd: 'usd' },
+    { account: 'account3', token: null, usd: 'usd' },
   ]);
-};
+}
